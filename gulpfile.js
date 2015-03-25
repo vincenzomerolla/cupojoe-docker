fb316b936775c6350c3e593929e08f9910a68d2e
Vincenzo Merolla <vincenzomerolla@aol.com>
2015-03-25 11:09:16 -0400

    Docker image being built

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var rsync = require('gulp-rsync');
+var packageJson = require('./package.json');
+var config = require('./config');
+
+gulp.task('deploy', function() {
+  var rsyncConfig = config.rsync;
+  return gulp.src(['**/*','!node_modules/**'])
+    .pipe(rsync({
+      progress: true,
+      username: rsyncConfig.username,
+      hostname: rsyncConfig.hostname,
+      destination: rsyncConfig.destination
+    }));
+})
+
+gulp.task('watch', function() {
+  gulp.watch('**/*.*', ['deploy']);
+});
+
+gulp.task('default', ['watch']);
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,93 @@
 var express = require('express');
 var router = express.Router();
 var Docker = require('dockerode');
+var swig = require('swig');
+var shell = require('shelljs');
+var tar = require('tar');
+var fstream = require('fstream');
+var fs = require('fs');
+
+
+var tarGzip = require('node-targz');
+
+var dirDest = fs.createWriteStream('archive.tar')
+
+
+function onError(err) {
+  console.error('An error occurred:', err)
+}
+
+function onEnd() {
+  console.log('Packed!')
+}
+
+
 
 var docker = new Docker();
-router.get('/', function(req, res, next) {
-//  res.render('index', { title: 'Express' });
- // docker.listContainers(function (err, containers) {
- //   res.json(containers);
- // });
-  docker.run('ubuntu', ['bash', '-c', 'uname -a'], process.stdout, function(err, data, container){
-    console.log(err, data, container);
-    res.json(container);
+router.route('/')
+.get(function(req, res, next) {
+
+  
+  // docker.createContainer({Image: 'maccam912/meanjs:latest', Cmd: ['/bin/bash', 'cd /Development/test_first_javascript/00_hello', 'mocha .'], name: 'mean-test2'}, function (err, container) {
+  //   console.log(err, container);
+  //   container.exec(function (err, data) {
+  //     console.log(err, data);
+  //     res.json(data);
+  //   });
+  // });
+
+  docker.run('5709d5b04409', ['bash', '-c', 'cd /Development/test_first_javascript/00_hello && mocha .'], res, function (err, data, container) {
+    console.log(data, container);
+    // res.json(data);
   });
 
+
+  //request back to test to update Image id
+  // PUT /test/id
+
+})
+
+.post(function(req, res, next){
+
+  var test = req.body;
+  var dockerfile = swig.renderFile('./DockerfileTemplate', test);
+  shell.mkdir('tmp');
+  dockerfile.to('tmp/Dockerfile');
+
+  // var packer = tar.Pack({ noProprietary: true })
+  //   .on('error', onError)
+  //   .on('end', onEnd);
+
+  // var reader = fstream.Reader({ path: 'tmp', type: 'Directory' });
+  
+  // reader.on('error', onError)
+  // reader.on('end', function (){  
+  //   shell.rm('-rf', 'tmp');
+    
+
+  //   docker.buildImage('archive.tar', { t: test._id }, function(err, response) {
+  //     if (err) onError(err);
+  //     console.log(response);
+  //     res.send(response);
+  //     //shell.rm('-f', 'archive.tar');
+  //   })
+  // });
+    
+  // reader.pipe(packer).pipe(dirDest);
+ 
+  tarGzip.compress({
+    source: 'tmp',
+    destination: 'archive.tar.gz',
+  }, function () {
+    docker.buildImage('archive.tar.gz', { t: 'cupojoe/test:' + Date.now() }, function(err, response) {
+      if (err) onError(err);
+      shell.rm('-rf', 'tmp');
+      shell.rm('-f', 'archive.tar.gz');
+      console.log(JSON.parse(response.body))
+      res.send(dockerfile);
+    })
+    
+  });
+
+})
 module.exports = router;
